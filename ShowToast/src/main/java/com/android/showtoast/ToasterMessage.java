34055958978cbc9e5b343fd9cc0367480cf50643
Suraj Negi <surajneginiit@gmail.com>
2022-06-29 14:36:15 +0530

    add : toast message

---
+++ ShowToast/src/main/java/com/android/showtoast/ToasterMessage.java
@@ -0,0 +1,12 @@
+package com.android.showtoast;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void show(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
